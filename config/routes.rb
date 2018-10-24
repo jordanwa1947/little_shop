c89da51470801eb7a9dc502691d2b56beb7c1171
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-24 15:01:22 -0600

    create login route, session controller, and login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/login', to: 'sessions#new'
+  #because it's not a resource its being saved in sessions, so we handroll
+  post '/login', to: 'sessions#create'
+  #trying to follow RESTful conventions
 end
