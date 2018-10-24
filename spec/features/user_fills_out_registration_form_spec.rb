5af30043748f9f2fd032b03570a6994d697a4200
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-24 16:13:20 -0600

    added test verifying unique emails for registration

--- spec/features/user_fills_out_registration_form_spec.rb
+++ spec/features/user_fills_out_registration_form_spec.rb
@@ -8,7 +8,7 @@ describe 'user visit registration page' do
       expect(current_path).to eq(new_user_path)
 
       fill_in "Name", with: "Jordan Whitten"
-      fill_in "Address", with: "Awesome street"
+      fill_in "Address", with: "8008 Awesome street"
       fill_in "City", with: "South Park"
       fill_in "State", with: "Denver"
       fill_in "Zip code", with: "00001"
@@ -19,6 +19,29 @@ describe 'user visit registration page' do
       click_on 'Create User'
 
       expect(page).to have_content('Welcome, Jordan Whitten')
+    end
+  end
+
+  describe 'user enters email address already in use' do
+    it "doesn't submit the form but stays on the registration page" do
+      User.create(name: 'Sherlock Holmes', address: '221 Baker street', city: 'London', state: 'opressed',
+      zip_code: '12345', email: 'AwesomeSauce@gmail.com', password: '123123',
+      password: '123123')
+
+      visit new_user_path
+
+      fill_in "Name", with: "Jordan Whitten"
+      fill_in "Address", with: "8008 Awesome street"
+      fill_in "City", with: "South Park"
+      fill_in "State", with: "Denver"
+      fill_in "Zip code", with: "00001"
+      fill_in "Email", with: "AwesomeSauce@gmail.com"
+      fill_in "Password", with: "123456"
+      fill_in "Password confirmation", with: "123456"
+
+      click_on 'Create User'
+
+      expect(current_path).to eq(new_user_path)
     end
   end
 end
