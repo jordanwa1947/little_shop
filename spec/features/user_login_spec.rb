ce1a17bae2f48c46eb575a573dd21b6992086ded
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-25 09:44:20 -0600

    update user login test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -3,17 +3,33 @@ require 'rails_helper'
 describe 'login process' do
   describe 'user can login as registered users' do
     it 'should succeed' do
-      name = "Geoffy"
-      password = 'test1234'
-      user = User.create(name: name, password: password)
 
+
+      name = "Jordan Whitten"
+      address = "8008 Awesome street"
+      city = "South Park"
+      state = "Colorado"
+      zip_code = "00001"
+      email = "AwesomeSauce@gmail.com"
+      password = "123456"
+      password_confirmation = "123456"
+
+      user = User.create(
+        name: name,
+        address: address,
+        city: city,
+        state: state,
+        zip_code: zip_code,
+        email: email,
+        password: password,
+        role: :registered_user,
+        status: :active)
       visit login_path
 
-      fill_in :name, with: name
-      fill_in :password, with: password
-      fill_in :confirm_password, with: password
-      click_on "Log in"
+      fill_in "Email", with: email
+      fill_in "Password", with: password
 
+      click_on "Log in"
       expect(current_path).to eq(user_path(user))
 
     end
