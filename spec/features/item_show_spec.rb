091179df91fbd62f621a93f40e9202ddedfa87cb
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-26 22:17:51 -0600

    create item show page test

--- spec/features/item_show_spec.rb
+++ spec/features/item_show_spec.rb
@@ -25,7 +25,7 @@ describe 'User visits an item show page' do
     @item_1 = @user_1.items.create(
       id: 1,
       name: "First Item",
-      price: 100.00,
+      price: 100,
       img_url: "https://static.grainger.com/rp/s/is/image/Grainger/12N166_AS01?$mdmain$",
       inventory_count: 5,
       description: "first description",
@@ -35,7 +35,7 @@ describe 'User visits an item show page' do
     @item_2 = @user_1.items.create(
       id: 2,
       name: "Second Item",
-      price: 200.00,
+      price: 200,
       img_url: "https://static.grainger.com/rp/s/is/image/Grainger/12N166_AS01?$mdmain$",
       inventory_count: 10,
       description: "second description",
@@ -45,7 +45,12 @@ describe 'User visits an item show page' do
 
   it 'should show that items details' do
 
-    
+    visit item_path(@item_1)
+    expect(page).to have_content("First Item")
+    expect(page).to have_content("$100")
+    expect(page).to have_content("FIRST USER")
+    expect(page).to have_content("In Stock: 5")
+    expect(page).to have_content("first description")
 
   end
 end
