542fed59ab4a8a6967893c96cc78059829719d97
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-27 17:51:33 -0600

    create basic cart page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,26 @@ class CartsController < ApplicationController
 
 
   def create
-    cart = session[:cart] || {}
-    cart[params[:item_id]] ||= 0
-    cart[params[:item_id]] += 1
-    session[:cart] = cart
+    @cart = session[:cart] || {}
+    @cart[params[:item_id]] ||= 0
+    @cart[params[:item_id]] += 1
+    session[:cart] = @cart
+
     redirect_to items_path
   end
 
+  # def create
+  #   item = Item.find(params[:item_id])
+  #   @cart.add_item(item)
+  #   session[:cart] = @cart.contents
+  #   redirect_to items_path
+  # end
+
+  def show
+    @cart = session[:cart]
+    @items = @cart.keys.map do |item_id|
+      Item.find(item_id.to_i)
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 user_1 = User.create(name:"Jim Jones", address:"123 Paradise Way" , city:"Jonestown" , state: "PA" , zip_code: 66667, password_digest: "password77" , email:"livin_to_die@example.com" )
 user_2 = User.create(name:"Jefferey Dahmer" , address:"321 Main Street" , city:"Milwaukee" , state:"WI" , zip_code: 54122 , password_digest: "password1" , email: "too_much_fun@example.com" )
-user_3 = User.create(name:"Ted Bundy" , address:"776 Example Ave" , city: "Chicago" , state:"IL" , zip_code:66754 , password_digest:"qwerty123" , email:"must_love_dogs@example.com" )
-user_4 = User.create(name:"Edward Gein" , address:"555 Murphy Road" , city:"Plainsville" , state:"WI" , zip_code:54321 , password_digest:"qdawg45" , email:"a_boys_best_friend@example.com" )
-user_5 = User.create(name:"Charles Manson" , address:"765 Hollywood blvd" , city:"Hollywood" , state:"CA" , zip_code: 78901, password_digest:"password65" , email:"helter_skelter@example.com" )
+user_3 = User.create(name:"Ted Bundy" , address:"776 Example Ave" , city: "Chicago" , state:"IL" , zip_code: 66754 , password_digest: "qwerty123" , email:"must_love_dogs@example.com" )
+user_4 = User.create(name:"Edward Gein" , address:"555 Murphy Road" , city:"Plainsville" , state:"WI" , zip_code: 54321 , password_digest: "qdawg45" , email:"a_boys_best_friend@example.com" )
+user_5 = User.create(name:"Charles Manson" , address:"765 Hollywood blvd" , city:"Hollywood" , state:"CA" , zip_code: 78901, password_digest: "password65" , email:"helter_skelter@example.com" )
 
 user_6 = User.create(name:"Felicity Huffman", address:"12 Not-the Way" , city:"Pleasantville" , state: "WA" , zip_code: 68927, password_digest: "password34" , email:"ruffnbuff@example.com" )
 user_7 = User.create(name:"Sally Field" , address:"6612 Purpose Street" , city:"Reno" , state:"NV" , zip_code: 76542 , password_digest: "password99" , email: "applepie@example.com" )
-user_8 = User.create(name:"Bette White" , address:"767 Main St" , city: "Santa Fe" , state:"NM" , zip_code:98537 , password_digest:"qwer67" , email:"cute_for_you@example.com" )
-user_9 = User.create(name:"Jennifer Lawerence" , address:"5785 Candle blvd" , city:"Augusta" , state:"MO" , zip_code:76678 , password_digest:"jlaw98" , email:"livin_loca889@example.com" )
-user_10 = User.create(name:"Margeret Thatcher" , address:"8896 Dev ave." , city:"New York" , state:"NY" , zip_code: 99876, password_digest:"password99" , email:"dream_gal@example.com" )
+user_8 = User.create(name:"Bette White" , address:"767 Main St" , city: "Santa Fe" , state:"NM" , zip_code: 98537 , password_digest: "qwer67" , email:"cute_for_you@example.com" )
+user_9 = User.create(name:"Jennifer Lawerence" , address:"5785 Candle blvd" , city:"Augusta" , state:"MO" , zip_code: 76678 , password_digest: "jlaw98" , email:"livin_loca889@example.com" )
+user_10 = User.create(name:"Margeret Thatcher" , address:"8896 Dev ave." , city:"New York" , state:"NY" , zip_code: 99876, password_digest: "password99" , email:"dream_gal@example.com" )
 
 
 
