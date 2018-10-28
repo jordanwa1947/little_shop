2e047e62789a53671c02df8b1170a6baf7bc086d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-27 20:16:03 -0600

    add flash message and cart counter incriment.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,24 @@
 class Cart
+  attr_reader :contents, :items
+
   def initialize(initial_contents)
     @contents = initial_contents || {}
+    @items = contents.keys.map do |item_id|
+      Item.find(item_id.to_i)
+    end
   end
+
+  def add_item(item_id)
+    contents[item_id.to_s] ||= 0
+    contents[item_id.to_s] += 1
+  end
+
+  def count_all
+    contents.values.sum
+  end
+
+  def quantity(item_id)
+    contents[item_id.to_s]
+  end
+
 end
