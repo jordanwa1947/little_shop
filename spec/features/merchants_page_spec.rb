c92bc53f1d9d058d8c7169f574cb15083c1fc412
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-28 12:42:03 -0600

    fixed navbar

--- spec/features/merchants_page_spec.rb
+++ spec/features/merchants_page_spec.rb
@@ -55,7 +55,7 @@ describe 'admin visits merchants page' do
       visit merchants_path
       click_link(@merchant_1.name)
 # in interest of time and security, I bypassed using merchant/show as
-# requested by the user story 
+# requested by the user story
       expect(current_path).to eq(admin_user_path(@merchant_1))
       expect(page).to have_content('Sherlock Holmes')
       expect(page).to have_content('merchant_user')
