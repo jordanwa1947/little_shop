45c0704a812ecff677b12af1b1ec40a2694e5db3
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-27 22:16:06 -0600

    remove commented method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,5 @@
 class CartsController < ApplicationController
 
-
-  # def create
-  #   @cart = session[:cart] || {}
-  #   @cart[params[:item_id]] ||= 0
-  #   @cart[params[:item_id]] += 1
-  #   session[:cart] = @cart
-  #
-  #   redirect_to carts_path
-  # end
-
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
