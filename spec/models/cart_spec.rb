7e54d1061aea7d074898f9c8e7a361aea7a5e1e7
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-29 11:02:04 -0600

    create cart spec

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Cart do
+
+  before(:each) do
+    name_1 = "First User"
+    address_1 = "123 Street"
+    city_1 = "Denver"
+    state_1 = "CO"
+    zip_code_1 = 80000
+    password_1 = "password1"
+    email_1 = "e@mail.com"
+
+    @user_1 = User.create(
+      name: name_1,
+      address: address_1,
+      city: city_1,
+      state: state_1,
+      zip_code: zip_code_1,
+      password: password_1,
+      email: email_1,
+      role: :merchant_user,
+      status: :active
+    )
+
+    @item_1 = @user_1.items.create(
+      id: 1,
+      name: "First Item",
+      price: 100,
+      img_url: "https://static.grainger.com/rp/s/is/image/Grainger/12N166_AS01?$mdmain$",
+      inventory_count: 5,
+      description: "first description",
+      status: :active
+    )
+
+    @item_2 = @user_1.items.create(
+      id: 2,
+      name: "Second Item",
+      price: 200,
+      img_url: "https://static.grainger.com/rp/s/is/image/Grainger/12N166_AS01?$mdmain$",
+      inventory_count: 10,
+      description: "second description",
+      status: :active
+    )
+  end
+
+  it ".count_all" do
+    cart = Cart.new({"1" => 2, "2" => 3})
+
+  end
+end
