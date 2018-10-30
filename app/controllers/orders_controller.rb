d9c0fce426b68cd3b5d59696fe74c1c1a8d91a1a
DanHutch <danhutch5280@gmail.com>
2018-10-30 12:05:30 -0600

    bugs fixed with order items in tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,10 +12,10 @@ class OrdersController < ApplicationController
       flash[:sucess] = "You have no items in your cart"
 
     elsif session[:cart] != nil
+      order = Order.create(user_id: session[:user_id], status: :pending)
       session[:cart].each do |key, value|
         item = Item.find(key.to_i)
-        order = Order.create(user_id: session[:user_id], status: :pending)
-        OrderItem.create(order_id: order.id, item_id: key.to_i, item_quantity: value, item_price: item.price)
+        order.order_items.create(item_id: key.to_i, item_quantity: value, item_price: item.price, fulfilled?: :false)
       end
       session[:cart].clear
       redirect_to profile_orders_path(current_user)
