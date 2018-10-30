12be168d0ea32f229ad9d1ea4ab9110cbc35c581
DanHutch <danhutch5280@gmail.com>
2018-10-30 15:13:36 -0600

    fixed more bugs

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  validates_presence_of :order_id, :item_id, :item_quantity, :item_price, :fulfilled?
-
+  validates_presence_of :order_id, :item_id, :item_quantity, :item_price
 
 end
