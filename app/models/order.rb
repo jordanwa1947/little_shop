e85f2211b774ed42a810d2e314fa912744850eeb
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-30 18:37:10 -0600

    adds total quantity method to order model, orders page

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,14 @@ class Order < ApplicationRecord
   validates_presence_of :user_id, :status
 
   enum status: %w(pending complete)
+
+  def total_price
+    items.inject(0) do |sum, item|
+      sum += item.price
+    end
+  end
+
+  def total_quantity
+    
+  end
 end
