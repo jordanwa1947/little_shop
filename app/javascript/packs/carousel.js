347245d70c66d49c78deccd286e8d96ad53cf406
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-29 01:04:49 -0700

    Only apply carousel javascript if on a page with the carousel

--- app/javascript/packs/carousel.js
+++ app/javascript/packs/carousel.js
@@ -1,51 +1,54 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   (function(doc) {
-    const images = doc.getElementsByClassName('carousel-image');
-    const prevBttn = doc.getElementById('prev-bttn');
-    const nextBttn = doc.getElementById('next-bttn');
-    let totalImgs = images.length;
-    let slide = 0;
-    let moving = false;
+    const carousel = doc.getElementById('carousel-cont');
+    if (carousel) {
+      const images = doc.getElementsByClassName('carousel-image');
+      const prevBttn = doc.getElementById('prev-bttn');
+      const nextBttn = doc.getElementById('next-bttn');
+      let totalImgs = images.length;
+      let slide = 0;
+      let moving = false;
 
-    prevBttn.addEventListener('click', moveToNext);
-    nextBttn.addEventListener('click', moveToPrev);
+      prevBttn.addEventListener('click', moveToNext);
+      nextBttn.addEventListener('click', moveToPrev);
 
-    function moveToNext() {
-      if(!moving) {
-        slide === (totalImgs - 1) ? slide = 0 : slide++;
-      }
-      moveCarouselTo(slide);
-    }
-
-    function moveToPrev() {
-      if(!moving) {
-        slide === 0 ? slide = totalImgs - 1 : slide--;
-      }
-      moveCarouselTo(slide);
-    }
-
-    function disableInteraction() {
-      moving = true;
-      setTimeout(function(){
-        moving = false
-      }, 500);
-    }
-
-    function moveCarouselTo(slide) {
-      if(!moving) {
-        disableInteraction();
-        images[0].classList.remove('initial');
-        for(let i = 0; i < totalImgs; i++) {
-          images[i].classList.remove('prev');
-          images[i].classList.remove('next');
-          images[i].classList.remove('active');
+      function moveToNext() {
+        if(!moving) {
+          slide === (totalImgs - 1) ? slide = 0 : slide++;
         }
-        let nextSlide, prevSlide;
-        slide === totalImgs - 1 ? nextSlide = 0 : nextSlide = slide + 1;
-        slide === 0 ? prevSlide = totalImgs - 1 : prevSlide = slide - 1;
-        images[slide].classList.add('active');
-        images[nextSlide].classList.add('next');
-        images[prevSlide].classList.add('prev');
+        moveCarouselTo(slide);
+      }
+
+      function moveToPrev() {
+        if(!moving) {
+          slide === 0 ? slide = totalImgs - 1 : slide--;
+        }
+        moveCarouselTo(slide);
+      }
+
+      function disableInteraction() {
+        moving = true;
+        setTimeout(function(){
+          moving = false
+        }, 500);
+      }
+
+      function moveCarouselTo(slide) {
+        if(!moving) {
+          disableInteraction();
+          images[0].classList.remove('initial');
+          for(let i = 0; i < totalImgs; i++) {
+            images[i].classList.remove('prev');
+            images[i].classList.remove('next');
+            images[i].classList.remove('active');
+          }
+          let nextSlide, prevSlide;
+          slide === totalImgs - 1 ? nextSlide = 0 : nextSlide = slide + 1;
+          slide === 0 ? prevSlide = totalImgs - 1 : prevSlide = slide - 1;
+          images[slide].classList.add('active');
+          images[nextSlide].classList.add('next');
+          images[prevSlide].classList.add('prev');
+        }
       }
     }
   }(event.target));
